a9deabf268f6b5a5b4d21f2edbd5b141f350844b
Yuliya Karuk <karuk.yuliya@gmail.com>
2024-07-11 17:46:51 +0300

    feat: install plugins for tests

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setup.ts',
+    coverage: {
+      provider: 'v8',
+      exclude: ['**/.eslintrc.cjs' ]
+    },
+  },
+});
