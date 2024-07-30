e7a7df4254bdf74246d06a9e4b40249adde0195f
Yuliya Karuk <karuk.yuliya@gmail.com>
2024-07-30 13:57:29 +0300

    fix: fix styles in nextjs

--- next.config.js
+++ next.config.js
@@ -5,14 +5,47 @@ const stylesPath = path.resolve('styles');
 export default {
   reactStrictMode: true,
   swcMinify: true,
-  webpack(config, options) {
+  // webpack(config, options) {
+  //   config.module.rules.push({
+  //     test: /\.svg$/,
+  //     use: ['@svgr/webpack'],
+  //   });
+
+  //   return config;
+  // },
+
+  webpack: (config, options) => {
+    const rules = config.module.rules
+      .find((rule) => typeof rule.oneOf === 'object')
+      .oneOf.filter((rule) => Array.isArray(rule.use));
+
+
+    rules.forEach((rule) => {
+      rule.use.forEach((moduleLoader) => {
+        if (
+          moduleLoader.loader !== undefined &&
+          moduleLoader.loader.includes('css-loader') &&
+          typeof moduleLoader.options.modules === 'object'
+        ) {
+          moduleLoader.options = {
+            ...moduleLoader.options,
+            modules: {
+              ...moduleLoader.options.modules,
+              exportLocalsConvention: 'camelCase',
+            },
+          };
+        }
+      });
+    });
+
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     });
 
-    return config;
-  },
+   return config;
+ },
+
 
   sassOptions: {
     includePaths: [stylesPath],
