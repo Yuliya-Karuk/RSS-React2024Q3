92957997cf88294fff45ea12e63fbf9f5d2de9c3
Yuliya Karuk <karuk.yuliya@gmail.com>
2024-08-08 15:48:16 +0300

    refactor: modify error boundary

--- app/components/ErrorButton/ErrorButton.tsx
+++ app/components/ErrorButton/ErrorButton.tsx
@@ -1,16 +1,9 @@
-import { useState } from 'react';
 import styles from './ErrorButton.module.scss';
 
 export const ErrorButton = () => {
-  const [hasError, setHasError] = useState(false);
-
   const throwError = () => {
-    setHasError(true);
-  };
-
-  if (hasError) {
     throw new Error('The site is broken!');
-  }
+  };
 
   return (
     <button type="button" className={styles.errorButton} onClick={throwError}>
--- app/root.tsx
+++ app/root.tsx
@@ -1,7 +1,9 @@
+import { ErrorComponent } from '@components/ErrorBoundary/ErrorComponent';
+import { ErrorButton } from '@components/ErrorButton/ErrorButton';
 import { Footer } from '@components/Footer/Footer';
 import { Header } from '@components/Header/Header';
 import { ThemeProvider } from '@contexts/themeProvider';
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import { Links, Meta, Outlet, Scripts, ScrollRestoration, useRouteError } from '@remix-run/react';
 import { store } from '@store/store';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
@@ -32,8 +34,29 @@ export default function App() {
         <Header />
         <Outlet />
         <Footer />
+        <ErrorButton />
         <ToastContainer position="top-center" autoClose={2000} className="Toastify" />
       </ThemeProvider>
     </Provider>
+  );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  console.error('LOG: This error was caught by Error Boundary', error);
+
+  return (
+    <html lang="en">
+      <head>
+        <title>Oh no! Error!</title>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <ErrorComponent />
+        <Scripts />
+      </body>
+    </html>
   );
 }
