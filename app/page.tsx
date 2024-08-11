81f6b967c7e541706365f20d5090b5e07e6aa7e2
Yuliya Karuk <karuk.yuliya@gmail.com>
2024-08-11 18:49:06 +0300

    fix: fix main loader

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
 import Home from '@components/Home/Home';
-import HomeWithLoader from '@components/HomeWithLoader/HomeWithLoader';
+import { Loader } from '@components/Loader/Loader';
+import { Suspense } from 'react';
+import styles from './page.module.scss';
 
 export default function Page({ searchParams }: { searchParams: { [key: string]: string } }) {
   return (
-    <HomeWithLoader>
+    <Suspense
+      key={`${searchParams.page}_${searchParams.query}`}
+      fallback={
+        <div className={styles.page}>
+          <Loader />
+        </div>
+      }
+    >
       <Home searchParams={searchParams} />
-    </HomeWithLoader>
+    </Suspense>
   );
 }
