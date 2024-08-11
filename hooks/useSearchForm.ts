58a36033c80dc93bb7a87572d2cd28b09e1cfbff
Yuliya Karuk <karuk.yuliya@gmail.com>
2024-08-11 20:03:19 +0300

    fix: fix query params for empty LS

--- hooks/useSearchForm.ts
+++ hooks/useSearchForm.ts
@@ -1,9 +1,10 @@
 import { useRouter } from 'next/router';
-import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import { useLocalStorage } from './useLocalStorage';
 
 export const useSearchForm = (inputRef: React.RefObject<HTMLInputElement>) => {
   const router = useRouter();
+
   const { getStorage, setStorage } = useLocalStorage();
   const [searchValue, setSearchValue] = useState<string>(getStorage() || '');
 
@@ -22,19 +23,6 @@ export const useSearchForm = (inputRef: React.RefObject<HTMLInputElement>) => {
     const newQuery = { page: '1', query: searchValue, ...(router.query.details && { details: router.query.details }) };
     router.push({ pathname: router.pathname, query: newQuery });
   };
-
-  useEffect(() => {
-    if (router.query.query !== getStorage()) {
-      const newQuery = {
-        page: '1',
-        query: getStorage(),
-        ...(router.query.details && { details: router.query.details }),
-      };
-      router.push({ pathname: router.pathname, query: newQuery });
-    }
-    // eslint-disable-next-line react-compiler/react-compiler
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   return { searchValue, handleInputChange, handleSubmit };
 };
